95a34a17cfe18b51d29e6595e055f60a2f44f7bc
pedroaugruiz <pedroaugustoruiz9@gmail.com>
2026-02-27 22:46:41 -0300

    Introducao Vite

---
+++ src/pages/sobre/index.jsx
@@ -0,0 +1,12 @@
+
+export default function Sobre () {
+    return(
+        <>
+            <h1>Sobre</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Laudantium quam blanditiis consectetur molestiae accusamus reprehenderit repellat suscipit hic, 
+                delectus atque saepe voluptatibus? Magni sed pariatur ullam, placeat quo ipsa optio?
+            </p>
+        </>
+    )
+}
