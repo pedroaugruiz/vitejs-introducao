4003d137308e3a77296cc231e09009497533a9f2
pedroaugruiz <pedroaugustoruiz9@gmail.com>
2026-03-02 22:45:24 -0300

    02/03/2026

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
-import './estilos/reset.css'
-import './estilos/globals.css'
-import App from './App.jsx'
+import App from './App.jsx';
+
+import './estilos/reset.css';
+import './estilos/globals.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
---
+++ src/pages/home/index.jsx
@@ -0,0 +1,16 @@
+
+function Home() {
+
+  return (
+    <>
+      <h1>Aula Introdução Vite JS</h1>
+      <h2>Preparo do projeto</h2>
+      <h3>Estilização</h3>
+      <p>Entendendo o uso de css dentro do projeto.</p>
+      <label>Exemplo Label</label>
+      <small>Exemplo Small</small>
+    </>
+  )
+}
+
+export default Home
